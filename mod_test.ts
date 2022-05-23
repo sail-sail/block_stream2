3e5ac0a224539420eed0edf2df979aa345f61367
Sail <151263555@qq.com>
2022-05-23 10:31:19 +0800

    first commit

--- mod_test.ts
+++ mod_test.ts
@@ -6,23 +6,23 @@ import { BlockStream2 } from "./mod.ts";
 
 Deno.test("BlockStream2", async function() {
   const b = new BlockStream2(16);
-	const fstr = fs.createReadStream("./mod_test.ts", { encoding: 'utf8' })
+  const fstr = fs.createReadStream("./mod_test.ts", { encoding: 'utf8' })
   fstr.pipe(b)
   b.resume();
-	let totalBytes = 0;
-	const stats = await stat("./mod_test.ts");
-	await new Promise((resolve, reject) => {
-		b.on("data", (data) => {
-			assertEquals(data.byteLength, 16, 'chunks should be 16 bytes long');
-			assert(Buffer.isBuffer(data));
-			totalBytes += data.length;
-		});
-		b.on("error", reject);
-		b.on('end', () => {
-			const expectedBytes = stats.size + (16 - stats.size % 16)
-			assertEquals(totalBytes, expectedBytes);
-			resolve(undefined);
-		});
-	});
+  let totalBytes = 0;
+  const stats = await stat("./mod_test.ts");
+  await new Promise((resolve, reject) => {
+    b.on("data", (data) => {
+      assertEquals(data.byteLength, 16, 'chunks should be 16 bytes long');
+      assert(Buffer.isBuffer(data));
+      totalBytes += data.length;
+    });
+    b.on("error", reject);
+    b.on('end', () => {
+      const expectedBytes = stats.size + (16 - stats.size % 16)
+      assertEquals(totalBytes, expectedBytes);
+      resolve(undefined);
+    });
+  });
 });
 
